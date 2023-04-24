18a62fd96b9e53aaeb72f4d1098e5c4b0d0da948
Arsam Bagheramiri <arsam5ba5@gmail.com>
2023-04-24 19:09:34 +0430

    single project component created

---
+++ HHB/components/Sections/singleProject/ProjectMainPhoto/index.vue
@@ -0,0 +1,80 @@
+<template>
+   <div class="projects-main-photo-wrapper">
+      <div class="projects-main-photo">
+         <img :src="`/_nuxt/static/images/slider/${singleProjectData.mainImage.image}`" :alt="singleProjectData.tabContent.specifications.name" loading="lazy"/>  
+      </div>
+      <div class="tap-to-down-button" @click="goDown">
+         <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M16.361,18h-3.361V1c0-.552-.447-1-1-1s-1,.448-1,1V18h-3.361c-.695,0-1.044,.852-.552,1.35l4.361,4.418c.305,.309,.799,.309,1.104,0l4.361-4.418c.492-.498,.143-1.35-.552-1.35Z"/></svg>
+      </div>
+   </div>  
+</template>
+
+<script>
+import { useSingleProject } from '~/src/stores/singleProject';
+export default {
+   setup () {
+      const singleProjectData = useSingleProject()
+      return singleProjectData
+   },
+   methods:{
+      goDown() {
+         const tabs = document.querySelector(".product-details-wrapper");
+         tabs.scrollIntoView();
+      }
+   },
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/global.scss';
+.projects-main-photo-wrapper {
+   width: 100%;
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   position: relative;
+
+   @include mq (lg) {
+      width: 100%;
+      left: 0;    
+   }
+
+   .projects-main-photo {
+      width: inherit;
+      height: inherit;
+
+      img {
+         height: 100%;
+         object-fit: cover;
+      }
+   }
+
+   .tap-to-down-button{
+      position: absolute;
+      bottom: 20px;
+      cursor: pointer;
+      animation: arrow 1.5s infinite;
+      
+      @keyframes arrow {
+         0%   {bottom: 0px;}
+         50%  {bottom: 20px;}
+         100% {bottom: 0px;}
+      }
+
+      svg {
+         width: 50px;
+         height: 50px;
+         fill: aliceblue;
+      }
+
+      &:hover {
+         svg {
+            fill: $secondary;
+         }
+      }
+
+   }
+}
+
+</style>
---
+++ HHB/components/Sections/singleProject/ProjectTabs/index.vue
@@ -0,0 +1,209 @@
+<template>
+   <div class="product-details-wrapper">
+         <div class="tabs-wrapper">
+            <button :class="[ 'tab' , descriptionTab ? 'active' : '' ]" value="description" @click="descriptionOpenTab">توضیحات</button>
+            <button :class="[ 'tab' , specificationsTab ? 'active' : '' ]" value="specifications" @click="specificationsTabOpenTab" >مشخصات</button>
+            <button :class="[ 'tab' , galleryTab ? 'active' : '' ]" value="gallery" @click="galleryOpenTab" >گالری</button>
+         </div>
+         <div class="tab-content-wrapper">
+            <div v-if="descriptionTab" class="tab-content description-tab">
+               <p v-html="singleProjectData.tabContent.description"></p>
+            </div>
+
+            <div v-if="specificationsTab" class="tab-content specifications-tab">
+               <ul>
+                  <li>اسم پروژه:<span style="font-weight: bold; margin: 0px 10px;"> {{ singleProjectData.tabContent.specifications.name }} </span></li>
+                  <li>موقعیت:<span style="font-weight: bold; margin: 0px 10px;"> {{ singleProjectData.tabContent.specifications.location }} </span></li>
+                  <li>کاربری:<span style="font-weight: bold; margin: 0px 10px;"> {{ singleProjectData.tabContent.specifications.user }} </span></li>
+                  <li>وضعیت:<span style="font-weight: bold; margin: 0px 10px;"> {{ singleProjectData.tabContent.specifications.situation }} </span></li>
+                  <li>سال ساخت:<span style="font-weight: bold; margin: 0px 10px;"> {{ singleProjectData.tabContent.specifications.constructionYear }} </span></li>
+               </ul>
+            </div>
+
+            <div v-if="galleryTab" class="tab-content gallery-tab">
+               <figure v-for="image in singleProjectData.tabContent.gallery" :key="image">
+                  <img :src="`/_nuxt/static/images/slider/${image}`" :alt="singleProjectData.tabContent.specifications.name" loading="lazy"/>
+               </figure>
+            </div>
+
+         </div>
+   </div>
+</template>
+
+<script>
+import { useSingleProject } from '~/src/stores/singleProject';
+export default ({
+   setup () {
+      const singleProjectData = useSingleProject()
+      return singleProjectData
+   },
+   data() {
+      return {
+         descriptionTab: true,
+         specificationsTab: false,
+         galleryTab: false,
+      }
+   },
+   methods: {
+      descriptionOpenTab(){
+         this.descriptionTab = true;
+         this.specificationsTab = false;
+         this.galleryTab = false;        
+      },
+      specificationsTabOpenTab(){
+         this.specificationsTab = true;
+         this.descriptionTab = false;
+         this.galleryTab = false;      
+      },
+      galleryOpenTab(){
+         this.galleryTab = true;
+         this.specificationsTab = false;
+         this.descriptionTab = false;        
+      },
+   },
+})
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/global.scss';
+.product-details-wrapper{
+   margin-top: 50px;
+   @include mq(lg) {
+      margin: 50px;
+   }
+}
+
+.tabs-wrapper {
+   display: flex;
+}
+
+.tab {
+   flex-basis: 50%;
+   border: 1px solid $primary;
+   background-color: $white;
+   padding: 0.5rem 1.5rem;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   border-top-left-radius: 12px;
+   border-top-right-radius: 12px;
+   font-size: 18px;
+   cursor: pointer;
+
+   @include mq(lg) {
+      flex-basis: 25%;
+      position: relative;
+      bottom: -1px;
+      padding: 1rem 2rem;
+   }
+
+   &.active {
+      border-bottom: none;
+   }
+}
+.tab-content-wrapper {
+   border: 1px solid $primary;
+   border-top: none;
+   padding: 2rem 1rem;
+   background-color: $white;
+   border-bottom-left-radius: 12px;
+   border-bottom-right-radius: 12px;
+
+   @include mq(md) {
+      padding: 48px;
+   }
+   @include mq(lg) {
+      border-top: 1px solid $primary;
+      border-top-left-radius: 12px;
+   }
+}
+.tab-content {
+
+   @include mq(md) {
+      font-size: 1rem;
+   }
+
+   .description {
+      font-size: 16px;
+      line-height: 1.9;
+      margin-bottom: 50px;
+
+      @include mq(md) {
+            font-size: 18px;
+      }
+   }
+}
+.feature-item-wrapper {
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: space-between;
+
+   .feature-item {
+      flex-basis: 100%;
+      list-style: none;
+      margin-bottom: 20px;
+      position: relative;
+      padding-right: 20px;
+
+      @include mq(md) {
+            flex-basis: 48%;
+      }
+
+      &:before {
+         content: '';
+         position: absolute;
+         display: block;
+         width: 5px;
+         height: 5px;
+         border-radius: 50%;
+         background-color: $primary;
+         right: 0;
+         top: 10px;
+      }
+   }
+}
+
+.specifications-tab{
+   ul {
+      list-style-type: none;
+
+      li {
+         margin-bottom: 20px;
+      }
+   }
+}
+
+.gallery-tab {
+   display: flex;
+   flex-direction: row;
+   flex-wrap: wrap;
+   align-items: center;
+   justify-content: space-between;
+   row-gap: 30px;
+   column-gap: 30px;
+
+   figure {
+      flex-basis: 100%;
+
+      @include mq(md) {
+         flex-basis: 47%;
+      }
+      @include mq(lg) {
+         flex-basis: 30%;
+      }
+      @include mq(xl) {
+         flex-basis: 22%;
+      }
+
+      img {
+         border-radius: 50px;
+         cursor: zoom-in;
+         
+         &:hover {
+            transform: scale(2);
+            transition: all 0.5s;
+         }
+      }
+   }
+}
+</style>
